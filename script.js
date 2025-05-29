dbc63e6bd87ed10ef1f6c31f53338a391555957b
ISHIKA SOLANKI <ishikasolanki682@gmail.com>
2025-05-29 11:32:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', () => {
+  search.classList.toggle('active');
+  input.focus();
+});
